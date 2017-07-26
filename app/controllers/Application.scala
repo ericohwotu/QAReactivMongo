a1579ab293e33935cb402f3bf9e7f357c0561a7f
ericohwotu <eric.ohwotu@outlook.com>
2017-07-26 23:46:14 +0100

    added parsing from json to class

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,11 +15,27 @@ import reactivemongo.play.json.collection.{JSONCollection, JsCursor}
 import JsCursor._
 import play.api.libs.json._
 import reactivemongo.api._
+import play.api.libs.functional.syntax._
 
 class Application @Inject() (val messagesApi: MessagesApi, val reactiveMongoApi: ReactiveMongoApi) extends Controller
   with MongoController with ReactiveMongoComponents with I18nSupport {
 
+  //the collection for the items
   def itemCol: Future[JSONCollection] = database.map(_.collection[JSONCollection]("ItemsCollection"))
+
+  //json readers
+  implicit val itemWriter: Reads[Item] = (
+    (__ \ "id").read[String] and
+      (__ \ "name").read[String] and
+      (__ \ "price").read[BigDecimal] and
+      (__ \ "description").read[String] and
+      (__ \ "manufacturer").read[String] and
+      (__ \ "warranty").read[Int] and
+      (__ \ "discount").read[BigDecimal] and
+      (__ \ "seller").read[String] and
+      (__ \ "image").read[String]
+  )(Item.apply _)
+
 
   def add = Action.async {
     val result = itemCol.flatMap(_.insert(Item.items.head))
@@ -36,9 +52,27 @@ class Application @Inject() (val messagesApi: MessagesApi, val reactiveMongoApi:
       item => Json.arr(item)
     }
 
-    array.map{
-      item => Ok(item)
+    list.map{
+      item => item.foreach{
+        i =>
+          println("**********************")
+          val nItem = i.validate[Item]
+          if(nItem.isSuccess)
+            Item.items += nItem.get
+          println(i.validate[Item])
+      }
     }
+
+
+
+    array.map{
+      item =>
+        println()
+        println("="*50)
+        println(Item.items)
+        Ok(item)
+    }
+
   }
 
   def getAll = Action.async {
--- app/models/Item.scala
+++ app/models/Item.scala
@@ -8,8 +8,8 @@ import scala.collection.mutable.ListBuffer
 
 
 case class Item(var id: String="NULL", var name: String, var price: BigDecimal, var description: String = "",
-                var manufacturer: String = "", var warranty: Option[Int] = Some(720),
-                var discount: Option[BigDecimal] = Some(0), var seller: String = "", var image: String = ""){
+                var manufacturer: String = "", var warranty: Int,
+                var discount: BigDecimal = 0, var seller: String = "", var image: String = ""){
 
 
   def replace(item: Item): Unit ={
@@ -36,17 +36,17 @@ object Item {
       "price" -> bigDecimal(9,2),
       "description" -> text,
       "manufacturer" -> text,
-      "warranty" -> optional(number),
-      "discount" -> optional(bigDecimal(9, 2)),
+      "warranty" -> default(number,720),
+      "discount" -> bigDecimal(9,2),
       "seller" -> text,
       "image" -> text
     )(Item.apply _)(Item.unapply _))
 
 
   val items: ListBuffer[Item] = ListBuffer(
-    Item(0.toString,"PS4", 245, "The ultimate in console gaming", "sony", Some(730)),
-    Item(1.toString,"XBOX ONE", 245, "The ultimate in console gaming", "Microsoft", Some(730)),
-    Item(2.toString,"Samsung Galaxy S8", 245, "The sexiest smartphone available", "Samsung", Some(730))
+    Item(0.toString,"PS4", 245, "The ultimate in console gaming", "sony", 730),
+    Item(1.toString,"XBOX ONE", 245, "The ultimate in console gaming", "Microsoft", 730),
+    Item(2.toString,"Samsung Galaxy S8", 245, "The sexiest smartphone available", "Samsung", 730)
   )
 
   //mongodb stuff
